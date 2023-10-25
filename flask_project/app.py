774a801bed0aaacd6a7ee1b32dc13cc7649a740f
Emmanuel-Begati <e.yakubu@alustudent.com>
2023-10-25 12:53:22 +0200

    almost done

--- flask_project/app.py
+++ flask_project/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -20,11 +20,39 @@ class Todo(db.Model):
 def index():
 
     if request.method == 'POST':
-        return 'Hello'
-    else:
-        return render_template('index.html')
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
 
-    return render_template('index.html')
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html' ,tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+    
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+        task = Todo.query.get_or_404(id)
+        if request.method == 'POST':
+            task.content = request.form('content')
+            db.session.commit()
+            return redirect('/')
+        else:
+            return render_template('update.html', task=task)
 
 if __name__ == "__main__":
     app.run(debug=True)
